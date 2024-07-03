62cceca50f39797c5ba30a95c02e53027520c405
WebFarhod <https://github.com/WebFarhod>
2024-07-03 11:26:33 +0500

    fix teacher

--- routes/teacherRoutes.ts
+++ routes/teacherRoutes.ts
@@ -6,32 +6,32 @@ import { Role } from "../enums/role";
 
 const routerTeacher = Router();
 
-routerTeacher.get("/rank", TeachersController.findAllRank);
-routerTeacher.post(
-  "/rank",
-//   authMiddleware,
-//   rolesMiddleware([Role.ADMIN]),
-  TeachersController.createRank
-);
-routerTeacher.post(
-  "/",
-//   authMiddleware,
-//   rolesMiddleware([Role.ADMIN]),
-  TeachersController.create
-);
+// routerTeacher.get("/rank", TeachersController.findAllRank);
+// routerTeacher.post(
+//   "/rank",
+// //   authMiddleware,
+// //   rolesMiddleware([Role.ADMIN]),
+//   TeachersController.createRank
+// );
+// routerTeacher.post(
+//   "/",
+// //   authMiddleware,
+// //   rolesMiddleware([Role.ADMIN]),
+//   TeachersController.create
+// );
 routerTeacher.get("/", TeachersController.findAll);
-routerTeacher.get("/:id", TeachersController.findOne);
-routerTeacher.put(
-  "/",
-//   authMiddleware,
-//   rolesMiddleware([Role.ADMIN]),
-  TeachersController.update
-);
-routerTeacher.delete(
-  "/",
-//   authMiddleware,
-//   rolesMiddleware([Role.ADMIN]),
-  TeachersController.remove
-);
+// routerTeacher.get("/:id", TeachersController.findOne);
+// routerTeacher.put(
+//   "/",
+// //   authMiddleware,
+// //   rolesMiddleware([Role.ADMIN]),
+//   TeachersController.update
+// );
+// routerTeacher.delete(
+//   "/",
+// //   authMiddleware,
+// //   rolesMiddleware([Role.ADMIN]),
+//   TeachersController.remove
+// );
 
 export default routerTeacher;
